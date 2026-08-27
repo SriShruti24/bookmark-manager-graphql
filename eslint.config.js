536d30676d2f45c198b5d9d9c20c4d73f0dd0cf9
Srishruti24 <sri.shruti24@gmail.com>
2026-08-27 19:58:45 +0530

    chore: initialize bookmark manager project

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+
+export default [
+  {
+    ignores: ["node_modules/**", "dist/**", "prisma/**"]
+  },
+  {
+    files: ["src/**/*.ts", "tests/**/*.ts"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module"
+      }
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin
+    },
+    rules: {
+      "@typescript-eslint/no-explicit-any": "error",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }]
+    }
+  }
+];
